2a60f751c08247eb0164c6de9009a79ef0f2954c
gantii <45544446+gantii@users.noreply.github.com>
2020-07-22 14:10:36 +0200

    added missing Speaker mappings

---
+++ src/NtFreX.Audio/Samplers/(Implementations)/(ChannelMapping)/FivePointZeroSampleChannelMapping.cs
@@ -0,0 +1,34 @@
+﻿using NtFreX.Audio.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NtFreX.Audio.Samplers
+{
+    internal sealed class FivePointZeroSampleChannelMapping : SampleChannelMapping
+    {
+        public override Speaker Speaker => throw new NotImplementedException();
+
+        public override byte[] GetFrontLeft(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetFrontRight(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(1 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetFrontCenter(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(2 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetSideLeft(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(3 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetSideRight(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(4 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+    }
+}
---
+++ src/NtFreX.Audio/Samplers/(Implementations)/(ChannelMapping)/SevenPointOneSurroundSampleChannelMapping.cs
@@ -0,0 +1,46 @@
+﻿using NtFreX.Audio.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NtFreX.Audio.Samplers
+{
+    internal sealed class SevenPointOneSurroundSampleChannelMapping : SampleChannelMapping
+    {
+        public override Speaker Speaker => throw new NotImplementedException();
+
+        public override byte[] GetFrontLeft(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetFrontRight(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(1 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetFrontCenter(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(2 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetLowFrequency(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(3 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetBackLeft(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(4 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetBackRight(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(5 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetFrontLeftOfCenter(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(6 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+        public override byte[] GetFrontRightOfCenter(byte[] sample, ushort bitsPerSample)
+        {
+            return sample.Skip(7 * bitsPerSample / 8).Take(bitsPerSample / 8).ToArray();
+        }
+    }
+}
