32dda970dee3a4ac02e5f55daeb0e5c687baf7e9
NtFreX <frederik@traber.org>
2020-08-06 22:06:39 +0200

    demo

---
+++ demo/NtFreX.Audio.Console/(Demos)/SinGeneratorDemo.cs
@@ -0,0 +1,41 @@
+﻿using NtFreX.Audio.Containers;
+using NtFreX.Audio.Infrastructure;
+using NtFreX.Audio.Math;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NtFreX.Audio.Console
+{
+    public class SinGeneratorDemo : IDemo
+    {
+        public string Name => nameof(SinGeneratorDemo);
+
+        public string Description => "Generates a sin tone with a given length and frequency and saves it to a given file";
+
+        public async Task RunAsync(CancellationToken cancellationToken)
+        {
+            System.Console.Write("Enter the file you want to create: ");
+            var file = System.Console.ReadLine();
+
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+
+            System.Console.Write("Enter the frequency: ");
+            var frequency = int.Parse(System.Console.ReadLine(), NumberFormatInfo.InvariantInfo);
+
+            System.Console.Write("Enter the length: ");
+            var length = int.Parse(System.Console.ReadLine(), NumberFormatInfo.InvariantInfo);
+
+            var sampleRate = 44100;
+            var format = new AudioFormat((uint)sampleRate, 64, 1, AudioFormatType.IeeFloat);
+            var data = WaveBuilder.Sin(format, frequency * sampleRate, length);
+            using var audio = await WaveEnumerableAudioContainerBuilder
+                .Build(format, data)
+                .ToFileAsync(file, FileMode.CreateNew, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
