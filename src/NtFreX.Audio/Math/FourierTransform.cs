01dd435e2c527784a772fc5a837af4e516456054
NtFreX <frederik@traber.org>
2020-08-13 17:28:26 +0200

    ubuntu docker container for debugg scenarios;

--- src/NtFreX.Audio/Math/FourierTransform.cs
+++ src/NtFreX.Audio/Math/FourierTransform.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 
 namespace NtFreX.Audio.Math
 {
@@ -9,18 +10,18 @@ namespace NtFreX.Audio.Math
     public static class FourierTransform
     {
         [return:NotNull]
-        public static CartesianCordinate[] Discrete([NotNull] CartesianCordinate[] input)
+        public static Complex[] Discrete([NotNull] Complex[] input)
         {
             _ = input ?? throw new ArgumentNullException(nameof(input));
 
             int n = input.Length;
-            var output = new CartesianCordinate[n];
+            var output = new Complex[n];
             for (int i = 0; i < n; i++)
             {
-                output[i] = new CartesianCordinate();
+                output[i] = default(Complex);
                 for (int j = 0; j < n; j++)
                 {
-                    var temp = CartesianCordinate.FromPolarCordinate(1, -2 * System.Math.PI * j * i / n);
+                    var temp = Complex.FromPolarCoordinates(1, -2 * System.Math.PI * j * i / n);
                     temp *= output[j];
                     output[i] += temp;
                 }
@@ -30,21 +31,21 @@ namespace NtFreX.Audio.Math
         }
 
         [return: NotNull]
-        public static CartesianCordinate[] Fast([NotNull] CartesianCordinate[] input)
+        public static Complex[] Fast([NotNull] Complex[] input)
         {
             _ = input ?? throw new ArgumentNullException(nameof(input));
 
             int n = input.Length;
-            CartesianCordinate[] output = new CartesianCordinate[n];
-            CartesianCordinate[] d1, d2, e1, e2;
+            Complex[] output = new Complex[n];
+            Complex[] d1, d2, e1, e2;
             if (n == 1)
             {
                 output[0] = input[0];
                 return output;
             }
             int k;
-            e1 = new CartesianCordinate[n / 2];
-            d1 = new CartesianCordinate[n / 2];
+            e1 = new Complex[n / 2];
+            d1 = new Complex[n / 2];
             for (k = 0; k < n / 2; k++)
             {
                 e1[k] = input[2 * k];
@@ -54,7 +55,7 @@ namespace NtFreX.Audio.Math
             e2 = Fast(e1);
             for (k = 0; k < n / 2; k++)
             {
-                d2[k] *= CartesianCordinate.FromPolarCordinate(1, -2 * System.Math.PI * k / n);
+                d2[k] *= Complex.FromPolarCoordinates(1, -2 * System.Math.PI * k / n);
             }
             for (k = 0; k < n / 2; k++)
             {
